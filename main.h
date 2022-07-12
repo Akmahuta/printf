989488a1e1b9413587c2f027fd19ffd2c1351efa
Monemax94 <mondayodoh94@gmail.com>
2022-07-12 10:59:37 -0700

    Header file updated

--- main.h
+++ main.h
@@ -71,7 +71,26 @@ int print_bigS(va_list l, flags_t *f);
 int print_address(va_list l, flags_t *f);
 
 /* print_percent */
-int print_percent(va_list l, flags_t *f);
+int print_percent(va_list l, flags_t *);
+
+
+/* width handler */
+int handle_write_char(char c, char buffer[],
+	int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[],
+	int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision,
+	int length, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length,
+	int width, int flags, char padd, char extra_c, int padd_start);
+
+int write_unsgnd(int is_negative, int ind,
+char buffer[],
+	int flags, int width, int precision, int size);
+
+/****************** UTILS ******************/
+int is_digit(char);
+
 
 
 #endif
